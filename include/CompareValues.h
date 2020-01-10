bc9b297e270be5a12896c2603283bdf605c530b3
Vincent Lohse <vincent.lohse@live.de>
2020-01-10 17:09:12 +0100

    Added idn & math import to comparevalues

--- include/CompareValues.h
+++ include/CompareValues.h
@@ -2,10 +2,12 @@
 #define CompareValues
 
 #include <iostream>
+#include <math.h>
 #include "Alloc.h"
 #include "Grid.h"
 #include "EMfield.h"
 #include "InterpDensSpecies.h"
+#include "InterpDensNet.h"
 
 void compareValues(struct grid grd, struct interpDensSpecies* idsGPU, struct interpDensSpecies* idsCPU);
 
