f64357bc0daa9e9e2ad892ce23ce1a31a69f2436
Vincent Lohse <vincent.lohse@live.de>
2020-01-10 19:30:50 +0100

    Separating batching code into new modules.

---
+++ include/ParticlesBatching.h
@@ -0,0 +1,25 @@
+#ifndef PARTICLES_BATCHING_H
+#define PARTICLES_BATCHING_H
+#define TPB 64
+
+#include <math.h>
+#include "Alloc.h"
+#include "Parameters.h"
+#include "PrecisionTypes.h"
+#include "Grid.h"
+#include "EMfield.h"
+#include "InterpDensSpecies.h"
+
+/** interpP2G for GPU*/
+void interpP2G_GPU_basic(struct particles*, struct interpDensSpecies*, struct grid*);
+
+/** particle mover for GPU with batching*/
+int mover_GPU_batch(struct particles*, struct EMfield*, struct grid*, struct parameters*);
+
+/** interpP2G for GPU with batching*/
+void interpP2G_GPU_batch(struct particles*, struct interpDensSpecies*, struct grid*);
+
+/** query amount of free memory available for use on GPU before splitting into batches*/
+size_t queryFreeMemoryOnGPU(void);
+
+#endif
